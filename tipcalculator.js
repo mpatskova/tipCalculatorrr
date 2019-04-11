bf53b65e1fbaea26d571815c6d9dfd8e6038a257
masik1995 <mpatskova@gmail.com>
2019-04-11 16:13:54 +0200

    changes was made

--- tipcalculator.js
+++ tipcalculator.js
@@ -3,12 +3,12 @@ function calculateTip() {
     var serviceQual = document.getElementById("qualityOfService").value;
     var numOfPeople = document.getElementById("amountOfPeople").value;
 
-    //validate input
+
     if (billAmt === "" || serviceQual == 0) {
         alert("Prosze wpisac kwote");
         return;
     }
-    //Check to see if this input is empty or less than or equal to 1
+
     if (numOfPeople === "" || numOfPeople <= 1) {
         numOfPeople = 1;
         document.getElementById("each").style.display = "none";
@@ -16,23 +16,17 @@ function calculateTip() {
         document.getElementById("each").style.display = "block";
     }
 
-    //Calculate tip
     var total = (billAmt * serviceQual) / numOfPeople;
-    //round to two decimal places
     total = Math.round(total * 100) / 100;
-    //next line allows us to always have two digits after decimal point
     total = total.toFixed(2);
-    //Display the tip
     document.getElementById("totalTip").style.display = "block";
     document.getElementById("tip").innerHTML = total;
 
 }
 
-//Hide the tip amount on load
 document.getElementById("totalTip").style.display = "none";
 document.getElementById("each").style.display = "none";
 
-//click to call function
 document.getElementById("calculate").onclick = function() {
     calculateTip();
 
